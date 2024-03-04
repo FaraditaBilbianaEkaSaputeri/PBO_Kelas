d2314ce3181d5ca0888de279a43e88de70e0ea8d
Bilbi <faraditabilbi@gmail.com>
2024-03-04 19:32:21 +0700

    Lat1

---
+++ KELAS/src/Cat.java
@@ -0,0 +1,15 @@
+
+public class Cat {
+	String name = "Miko";
+	String color = "Orange";
+	int age = 5;
+	
+	void suaraSenang() {
+		System.out.println("*Miawww*");
+	}
+	
+	void suaraMarah() {
+		System.out.println("*MIAWWWW!!!!*");
+	}
+
+}
